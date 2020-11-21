d27ebb665972334f9e6022857f135f2248fdb723
b30wulffz <shlokpandey123@gmail.com>
2020-11-21 14:29:35 +0530

    Minimal homepage ui

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   BrowserRouter as Router,
   Switch,
@@ -6,18 +6,70 @@ import {
   Link,
   Redirect,
 } from "react-router-dom";
+import FadeIn from "react-fade-in";
+import Lottie from "react-lottie";
+import { makeStyles } from "@material-ui/core/styles";
 
 import "./App.css";
 import Navbar from "./components/Navbar";
+
+// import * as loader from "./components/coder-coding.json";
+import * as loader from "./components/cute-cat.json";
 
 import Home from "./home/Home";
 import TspSimulation from "./TspSimulation";
 import LGSimulation from "./lerchs-grossman/LGSimulation";
 
+// height: 100vh;
+// width: 100%;
+// display: flex;
+// justify-content: center;
+// align-items: center;
+const useStyles = makeStyles((theme) => ({
+  loading: {
+    height: "100vh",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+}));
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: loader.default,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
 function App() {
-  return (
+  const classes = useStyles();
+
+  const [load, setLoad] = useState({
+    loaded: false,
+    currClass: "",
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad({ loaded: true, currClass: "done" });
+    }, 2000);
+  }, []);
+
+  return !load.loaded ? (
+    // <Example type="bubbles" color="red" />
+    <div className={classes.loading}>
+      <FadeIn>
+        <div className="loader">
+          <Lottie options={defaultOptions} height={220} width={220} />
+        </div>
+      </FadeIn>
+    </div>
+  ) : (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Switch>
         <Route path="/" exact>
           <Home />
--- src/home/Home.js
+++ src/home/Home.js
@@ -12,10 +12,78 @@ import logoImage from "./logo.svg";
 
 const useStyles = makeStyles((theme) => ({
   logoImage: {},
-  root: {
+  lowerHalf: {
     height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    padding: "50px",
+    boxSizing: "border-box",
+  },
+  lowerHalfHeadingSection: {
+    flex: "1",
+  },
+  lowerHalfHeading: {
+    textAlign: "center",
+  },
+  tilesSection: {
+    flex: "6",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-around",
+    // background: "red",
+  },
+  tilesPart: {
+    flex: "1",
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-around",
+    // background: "yellow",
+  },
+  tile: {
+    flex: 1,
+    background: "blue",
+    margin: "30px",
+    height: "200px",
+    borderRadius: "20px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+    color: "#fff",
+    transition: "0.3s ease",
+    fontSize: "30px",
+    opacity: "0.8",
+    textAlign: "center",
+    "&:hover": {
+      boxShadow: "inset 0 0 0 2000px rgba(36, 36, 36, 0.3)",
+      opacity: "0.9",
+    },
+  },
+  tile1: {
+    background: "linear-gradient(-45deg, #F095ff, #f64848)",
+  },
+  tile2: {
+    background: "linear-gradient(-45deg, #F6FBA2, #20DED3)",
+    // background: "linear-gradient(-45deg, #20E2D7, #F9FEA5)",
+    // background-image: linear-gradient(-225deg, #20E2D7 0%, #F9FEA5 100%);
+  },
+  tile3: {
+    background: "linear-gradient(-45deg, #4facfe, #00f2fe)",
+    // background-image: linear-gradient(-225deg, #20E2D7 0%, #F9FEA5 100%);
+  },
+  tile4: {
+    background: "linear-gradient(-45deg, #FB7BA2, #FCE043)",
+    // background: "linear-gradient(-45deg, #ff5f6d, #ffc371)",
+    // background: "linear-gradient(-45deg, #f6d365, #fda085)",
+    // background: "linear-gradient(-45deg, #e0c3fc, #8ec5fc)",
+  },
+  algoInfoBody: {
+    width: "80vw",
   },
   algotile: {
+    width: "100px",
     padding: "20px",
     background: "red",
   },
@@ -37,31 +105,30 @@ const HomeComponent = () => {
 
   return (
     <>
-      <Grid
-        container
-        className={classes.root}
-        justify="space-around"
-        alignItems="center"
-      >
-        <Grid item container xs={4} sp>
-          <Grid item xs={12}>
-            <Paper className={classes.algotile}>Algo 1</Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.algotile}>Algo 1</Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.algotile}>Algo 1</Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.algotile}>Algo 1</Paper>
-          </Grid>
-        </Grid>
-        <Grid item xs={4}>
-          <img src={logoImage} className={classes.logoImage} />
-        </Grid>
-      </Grid>
-      <ParticlesBg type={currentAnimation} bg={true} />
+      <div className={classes.lowerHalf}>
+        <div className={classes.lowerHalfHeadingSection}>
+          <h1 className={classes.lowerHalfHeading}>Learn something new!</h1>
+        </div>
+        <div className={classes.tilesSection}>
+          <div className={classes.tilesPart}>
+            <div className={`${classes.tile} ${classes.tile1}`}>
+              Travelling Salesman Problem
+            </div>
+            <div className={`${classes.tile} ${classes.tile2}`}>
+              Boid's Algorithm
+            </div>
+          </div>
+          <div className={classes.tilesPart}>
+            <div className={`${classes.tile} ${classes.tile3}`}>
+              Lerchs Grossman Algorithm
+            </div>
+            <div className={`${classes.tile} ${classes.tile4}`}>
+              Cryptography
+            </div>
+          </div>
+        </div>
+      </div>
+      <ParticlesBg type={"polygon"} bg={true} />
     </>
   );
 };
